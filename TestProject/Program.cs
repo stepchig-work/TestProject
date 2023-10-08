997660f5862a8b307d5c477842cc073f37f916db
Stepan <Stepan@DESKTOP-N067JVE>
2023-10-08 12:47:31 +0200

    Add project files.

---
+++ TestProject/Program.cs
@@ -0,0 +1,23 @@
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+
+app.MapGet("/healthcheck", () =>
+{
+	return HttpStatusCode.OK;
+});
+
+app.Run();
